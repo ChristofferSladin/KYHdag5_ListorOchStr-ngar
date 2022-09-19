ddf9e25b4d9ab29a1dbe4958c3036e761cc82c35
Christoffer S <christoffer.sladin@hotmail.com>
2022-09-19 20:42:19 +0200

    Add project files.

---
+++ ArrayList4Integers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYHdag5_ListorOchSträngar
+{
+    internal class ArrayList4Integers
+    {
+
+        public void RunList4Integers()
+        {
+            List<int> numList = new List<int>();
+            Console.WriteLine("Ange 4 tal");
+
+            //numList.Add(int.Parse(Console.ReadLine()));
+            //numList.Add(int.Parse(Console.ReadLine()));
+            //numList.Add(int.Parse(Console.ReadLine()));
+            //numList.Add(int.Parse(Console.ReadLine()));
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                numList.Add(int.Parse(Console.ReadLine()));
+
+            }
+            Console.WriteLine(numList);
+        }
+
+        public void RunArray4Integers()
+        {
+            int[] arrayOfIntegers = new int[4];
+            int maxValue = 0;
+            
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Ange ett tal");
+                arrayOfIntegers[i] = int.Parse(Console.ReadLine());
+            } 
+
+            for (int i = 0; i < arrayOfIntegers.Length; i++)
+            {
+                maxValue = arrayOfIntegers.Max();
+            }
+
+            Console.WriteLine(maxValue);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using KYHdag5_ListorOchSträngar;
+
+var app1 = new ArrayList4Integers();
+app1.RunArray4Integers();
+app1.RunList4Integers();
