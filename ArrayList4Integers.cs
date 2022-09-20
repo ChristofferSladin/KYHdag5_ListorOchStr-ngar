d8dba7bdffa6925cde8eb0b311c79e9918a4bc9d
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2022-09-20 14:23:49 +0200

    50% av labbarna avklarade

--- ArrayList4Integers.cs
+++ ArrayList4Integers.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,233 @@ namespace KYHdag5_ListorOchSträngar
 {
     internal class ArrayList4Integers
     {
+        public void Labb3()
+        {
+            var listWithStrings = new List<string>();
+
+            listWithStrings.Add("Kalle");
+            listWithStrings.Add("aa");
+            listWithStrings.Add("ada");
+            listWithStrings.Add("Nisse");
+
+            foreach (string name in listWithStrings)
+            {
+                if (name.StartsWith("a") && name.EndsWith("a") && name.Length < 4)
+                {
+                    Console.WriteLine($"De namn som börjar och slutar med a och har minst 2 chars är: {name}");
+                }
+            }
+
+        }
+        public void TestLabbBarn()
+        {
+            var allaBarn = new List<string>();
+
+            allaBarn.Add("Filippa");
+            allaBarn.Add("Linnea");
+            allaBarn.Add("Christoffer");
+
+            allaBarn[2] = "Chrille";
+
+            string namn11 = allaBarn[2];
+
+            var playersInNYrangers = new List<string>();
+
+            var allYears = new List<int>();
+
+            allYears.Add(1999);
+            allYears.Add(2000);
+
+            foreach (int year in allYears)    // Use foreach when looping thru lists
+            {
+                Console.WriteLine($"{year}");
+            }
+        }
+
+        public void Labb00()
+        {
+            var numList = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Mata in ett nr");
+                numList.Add(int.Parse(Console.ReadLine()));
+            }
+            numList.Sort();
+            Console.WriteLine(numList.Max());
+        }
+
+
+
+        public void Labb0()
+        {
+            int[] numbers = new int[4];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Ange ett tal");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            var biggestSoFar = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > biggestSoFar)
+                {
+                    biggestSoFar = num;
+                }
+            }
+            Console.WriteLine($"Detta är det största angivna talet {biggestSoFar}");
+
+
+        }
+
+
+        public void Labb2()
+        {
+            Console.WriteLine("Ange hur många tal du vill mata in");
+            var antalTal = int.Parse(Console.ReadLine());
+
+            int[] numberArray = new int[antalTal];
+            var sum = 0;
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                Console.WriteLine("Ange ett tal");
+                numberArray[i] = int.Parse(Console.ReadLine());
+
+                sum += numberArray[i];
+            }
+
+            Console.WriteLine($"Detta är totala summan av alla talen {sum}");
+
+        }
+
+
+        public void Labb4()
+        {
+            bool run = true;
+
+            var listOfNumbers = new List<int>();
+
+            while (run)
+            {
+                Console.WriteLine("mata in ett val (t) eller avsluta med (n)");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "t":
+
+                        Console.WriteLine("Mata in ett tal");
+                        listOfNumbers.Add(int.Parse(Console.ReadLine()));
+                        break;
+
+                    case "n":
+
+                        run = false;
+                        Console.WriteLine("Du avlsutade programmet");
+
+                        foreach (int tal in listOfNumbers)
+                        {
+                            Console.WriteLine($"De tal som du matade in var: {tal}\n");
+                        }
+                        break;
+                }
+            }
+        }
+        public void ListWalkThrough()
+        {
+            var allLegendaryPlayers = new List<string>();
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine("1. Skapa spelare");
+                Console.WriteLine("2. Lista spelare");
+                Console.WriteLine("3. Ta bort spelare");
+                Console.WriteLine("4. Ändra spelare");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("---------------------------");
+
+                var menyVal = Console.ReadLine();
+
+                switch (menyVal)
+                {
+                    case "1":
+                        Console.WriteLine("Ange namn på nya spelare");
+                        var name = Console.ReadLine();
+
+                        allLegendaryPlayers.Add(name);
+                        break;
+
+                    case "2":
+                        foreach (string names in allLegendaryPlayers)
+                            Console.WriteLine(names);
+                        break;
+
+                    case "3":
+
+                        Console.WriteLine("Ange siffra ");
+
+                        var index = 0;
+                        foreach (var names in allLegendaryPlayers)
+                        {
+                            index++;
+                            Console.WriteLine($"{index} {names}");
+                        }
+
+                        break;
+
+                    case "4":
+
+
+                        break;
+
+                    case "5":
+                        run = false;
+
+                        break;
+
+                }
+            }
+
+
+
+
+        }
+        public void Labb1()
+        {
+            int[] nums = new int[4];
+
+            nums[0] = 21;
+            nums[1] = 32;
+            nums[2] = 22;
+            nums[3] = 99;
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            string[] weekDays = { "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            while (true)
+            {
+                Console.WriteLine("Ange ett veckodags nummer mellan 1-7");
+                int dagNummer = int.Parse(Console.ReadLine());
+
+                string weekDayNames = weekDays[dagNummer - 1];
+
+                Console.WriteLine($"Du angav {dagNummer} på den platsen ligger {weekDayNames}");
+            }
+
+
+
+        }
 
         public void RunList4Integers()
         {
@@ -32,13 +262,13 @@ namespace KYHdag5_ListorOchSträngar
         {
             int[] arrayOfIntegers = new int[4];
             int maxValue = 0;
-            
+
 
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Ange ett tal");
                 arrayOfIntegers[i] = int.Parse(Console.ReadLine());
-            } 
+            }
 
             for (int i = 0; i < arrayOfIntegers.Length; i++)
             {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿using KYHdag5_ListorOchSträngar;
 
 var app1 = new ArrayList4Integers();
-app1.RunArray4Integers();
-app1.RunList4Integers();
+//app1.RunArray4Integers();
+//app1.RunList4Integers();
+//app1.Labb1();
+//app1.ListWalkThrough();
+//app1.Labb4();
+//app1.Labb2();
+//app1.Labb0();
+//app1.Labb00();
+//app1.Labb3();
+//app1.TestLabbBarn();
